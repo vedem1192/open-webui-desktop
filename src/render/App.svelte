c57cea593255908af07352d65ce5e004de94aba9
Timothy Jaeryang Baek <tim@openwebui.com>
2025-01-08 19:15:46 -0800

    feat: vite+svelte+tailwind setup

---
+++ src/render/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    console.log("Mounted");
+  });
+</script>
+
+<main>
+  <div class=" text-xl font-bold">Open WebUI App</div>
+</main>
